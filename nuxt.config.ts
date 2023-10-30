fedcdcf98fcfbce20afee46040788e8cf442058f
Alex Tana <alextana@Alexs-Air.home>
2023-10-30 18:10:22 +0000

    prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
-    ORIGIN: process.env.ORIGIN,
+    AUTH_ORIGIN: process.env.AUTH_ORIGIN,
     NUXT_SECRET: process.env.NUXT_SECRET,
   },
   build: {
