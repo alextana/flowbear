d9d4a6a9ebf73b154900cb3ed22a949a04d4e797
Alex Tana <alextana@Alexs-Air.home>
2023-11-01 13:23:05 +0000

    add signin marketing page

--- layouts/marketing.vue
+++ layouts/marketing.vue
@@ -1,3 +1,12 @@
-<template>
+<template v-if="status !== 'authenticated'">
   <slot />
 </template>
+
+<script setup>
+const { status } = useAuth()
+onBeforeMount(async () => {
+  if (status.value === 'authenticated') {
+    await navigateTo('/')
+  }
+})
+</script>
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,5 +1,20 @@
-<template>marketing page!!</template>
+<template>
+  <div
+    class="w-screen text-center h-screen grid place-content-center container mx-auto"
+  >
+    <h1 class="text-7xl font-extrabold">Notelap</h1>
+    <p class="mt-2">Marketing page coming soon</p>
+    <UiSeparator class="my-4" />
+    <div class="sign-in text-center mx-auto">
+      <div v-if="status === 'unauthenticated'">
+        <UiButton kind="primary" @click="signIn('google')">sign in</UiButton>
+      </div>
+    </div>
+  </div>
+</template>
 <script setup>
+const { status, data, signIn, signOut } = useAuth()
+
 definePageMeta({
   layout: 'marketing',
 })
